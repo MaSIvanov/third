1660f3a97610b178f6d17dce11a5da8251a1f25f
Maksim <henen4jin@mail.ru>
2025-02-07 15:21:52 +0300

    first

---
+++ app.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_date():
+    dt = datetime.now()
+    dt_format = dt.strftime("%d/%m/%y")
+    return "Текущая дата: {}".format(dt_format)
+
+
+if __name__ == "__main__":
+    app.run()
