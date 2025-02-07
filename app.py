11151779bb0b80b5ff9414b237207b37750b579a
MaSIvanov <119003846+MaSIvanov@users.noreply.github.com>
2025-02-07 17:30:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def get_date():
     dt = datetime.now()
     dt_format = dt.strftime("%d/%m/%y")
-    return "Привет, Максим! Текущая дата: {}".format(dt_format)
+    return "Привет, Максим!"
 
 
 if __name__ == "__main__":
